f27d8374d4179cd929dd9fddfeb1227a9c75abf3
Manuel Fernández <manueleloyfernandez@gmail.com>
2022-07-26 21:15:42 -0300

    primeros pasos calculadora

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let resultadoSuma = document.getElementById("resultado");
+
+
+
+function sumar(num1, num2) {
+    
+    return resultadoSuma.textContent = `El resultado es ${num1 + num2}`;
+
+    };
+
+
+sumar(4, 7);
